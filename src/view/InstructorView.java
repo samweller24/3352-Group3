c98adbde1051b4b95d17bb4151f10c4dda23d8e1
samweller24 <samweller24@gmail.com>
2021-11-30 12:49:38 -0500

    Adding views

--- src/view/InstructorView.java
+++ src/view/InstructorView.java
@@ -1,5 +1,10 @@
 package src.view;
 
 public class InstructorView {
-    
+    public void printInstructorDetails(String userName, String email, String[] taughtCourses){
+        System.out.println("Instructor: ");
+        System.out.println("Username: " + userName);
+        System.out.println("Email: " + email);
+        System.out.println("Taught Courses: " + taughtCourses);
+    }
 }
--- src/view/StudentView.java
+++ src/view/StudentView.java
@@ -1,5 +1,10 @@
 package src.view;
 
 public class StudentView {
-    
+    public void printStudentDetails(String userName, String email, String[] courseEnrollment){
+        System.out.println("Student: ");
+        System.out.println("Username: " + userName);
+        System.out.println("Email: " + email);
+        System.out.println("Enrolled Courses: " + courseEnrollment);
+    } 
 }
