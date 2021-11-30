9f7eb6bde90d29a9e3a5bf4a0d029b1b1fc1fcfc
samweller24 <samweller24@gmail.com>
2021-11-30 13:41:42 -0500

    Controller done

--- src/controller/InstructorController.java
+++ src/controller/InstructorController.java
@@ -1,5 +1,41 @@
 package src.controller;
+import src.model.Instructor;
+import src.view.InstructorView;
 
 public class InstructorController {
-    
+    private Instructor model;
+    private InstructorView view;
+     
+    public InstructorController(Instructor model, InstructorView view){
+        this.model = model;
+        this.view = view;
+    }
+     
+    public void setInstructorUserName(String userName){
+        model.setUserName(userName);		
+    }
+     
+    public String getInstructorName(){
+        return model.getUserName();		
+    }
+     
+    public void setInstructorEmail(String email){
+        model.setEmail(email);		
+    }
+     
+    public String getInstructorEmail(){
+        return model.getEmail();		
+    }
+
+    public void addCourse(String course){
+        model.addCourseTaught(course);		
+    }
+     
+    public String[] getTeaches(){
+        return model.getTeaches();		
+    }
+     
+    public void updateView(){				
+        view.printInstructorDetails(model.getUserName(), model.getEmail(), model.getTeaches());
+    }	
 }
--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -1,5 +1,41 @@
 package src.controller;
+import src.model.Student;
+import src.view.StudentView;
 
 public class StudentController {
-    
+    private Student model;
+    private StudentView view;
+     
+    public StudentController(Student model, StudentView view){
+        this.model = model;
+        this.view = view;
+    }
+     
+    public void setStudentUserName(String userName){
+        model.setUserName(userName);		
+    }
+     
+    public String getStudentName(){
+        return model.getUserName();		
+    }
+     
+    public void setStudentEmail(String email){
+        model.setEmail(email);		
+    }
+     
+    public String getStudentEmail(){
+        return model.getEmail();		
+    }
+
+    public void addCourse(String course){
+        model.addEnrolledCourse(course);		
+    }
+     
+    public String[] getCourseEnrollment(){
+        return model.getCourseEnrollment();		
+    }
+     
+    public void updateView(){				
+        view.printStudentDetails(model.getUserName(), model.getEmail(), model.getCourseEnrollment());
+    }	
 }
